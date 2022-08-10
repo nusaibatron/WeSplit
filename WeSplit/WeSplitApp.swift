ff92bb9b1756fa4ee60c5cea4fab5eecc28e7b02
nusaibatron <nusaiba@berkeley.edu>
2022-08-10 10:17:53 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nusaiba Rahman on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
